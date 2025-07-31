48e591c6104befba2a7bdfe750fd686a8572e496
25LP2339 25LP2339 <25LP2339@pinecones-iMac-2.local>
2025-07-31 09:14:58 +0800

    sec

---
+++ src/app/(main)/explore/components/ExploreHeader.tsx
@@ -0,0 +1,19 @@
+import { Search } from "lucide-react"; // эсвэл өөр icon ашиглаж болно
+
+export const ExploreHeader = () => {
+  return (
+    <div className="text-black">
+      <h2 className="mb-4 text-xl font-semibold">Explore creators</h2>
+      
+      <div className="relative w-full max-w-md">
+        <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" size={18} />
+
+        <input
+          type="text"
+          placeholder="Search name"
+          className="pl-10 pr-4 py-2 w-full border border-neutral-400 rounded-md text-black focus:outline-none"
+        />
+      </div>
+    </div>
+  );
+};
