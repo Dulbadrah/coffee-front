87080c7076ab46de369b0c3647586af70da678d6
odkhuu <odkhuu06@gmail.com>
2025-08-04 22:06:44 +0800

    hho

--- src/app/(main)/exploreMore/components/ExploreBgImg.tsx
+++ src/app/(main)/exploreMore/components/ExploreBgImg.tsx
@@ -2,23 +2,22 @@ import Image from "next/legacy/image";
 import { ExploreMoreLeft } from "./ExploreMoreLeft";
 import { ExploreUrl } from "./ExploreUrl";
 import { ExploreMoreRight } from "./ExploreMoreRight";
- 
+
 export const ExploreBgImg = () => {
   return (
     <div className="relative">
-      <Image
-        src="/frame.png"
-        alt="Profile"
-        layout="responsive"
-        width={1440}
-        height={319}
-      />
- 
-    
-      <ExploreMoreLeft />
-      <ExploreUrl/>
-      <ExploreMoreRight/>
+      <img src="/frame.png" alt="frame" width={"100%"} height="100%" />
+
+      <div className="absolute z-50 top-36 left-5 flex w-full gap-8 p-4">
+        <div className="flex-1/2">
+          <ExploreMoreLeft />
+          <ExploreUrl />
+        </div>
+        <div className="flex-1/2">
+
+        <ExploreMoreRight />
+        </div>
+      </div>
     </div>
   );
 };
- 
--- src/app/(main)/exploreMore/components/ExploreMoreRight.tsx
+++ src/app/(main)/exploreMore/components/ExploreMoreRight.tsx
@@ -1,10 +1,21 @@
 import Image from "next/legacy/image";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import QRCode from "react-qr-code";
 
 export const ExploreMoreRight = () => {
   const items = Array.from({ length: 4 });
   return (
-    <div className="w-[632px] h-[509px] absolute top-[369px] right-[50px] border bg-white p-4 rounded-sm">
-      <h2 className="text-lg font-semibold mb-1">Buy Space ranger a Coffee</h2>
+    <div className="w-full h-[509px]  border bg-white p-4 rounded-sm overflow-hidden ">
+      <h2 className="text-lg font-semibold">Buy Space ranger a Coffee</h2>
       <p>Select amount:</p>
       <div className="flex gap-2 mt-2">
         {items.map((_, i) => (
@@ -21,15 +32,41 @@ export const ExploreMoreRight = () => {
       </div>
       <div>
         <h2>Enter BuyMeCoffee or social acount URL:</h2>
-        <input className="w-[580px] h-10 border rounded-sm" type="text" placeholder="buymeacoffee.com/" />
+        <input
+          className="w-[580px] h-10 border rounded-sm"
+          type="text"
+          placeholder="buymeacoffee.com/"
+        />
       </div>
       <div>
         <h2>Special message:</h2>
-        <input className="w-[580px] h-[131px] border rounded-sm" type="text" placeholder="Please write your message here" />
-        </div>
-        <div>
-            <button>Support</button>
-        </div>
+        <input
+          className="w-[580px] h-[131px] border rounded-sm"
+          type="text"
+          placeholder="Please write your message here"
+        />
+      </div>
+
+      <Dialog>
+        <form>
+          <DialogTrigger asChild>
+            <button className="w-full  bg-black text-white rounded- text-sm p-2 rounded-[4px] mt-4">
+              Support
+            </button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+            </DialogHeader>
+            <QRCode
+              size={256}
+              style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+              value={"asdadnjdsf"}
+              viewBox={`0 0 256 256`}
+            />
+          </DialogContent>
+        </form>
+      </Dialog>
     </div>
   );
 };
