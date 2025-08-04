ad033e62b3538554e47b25b0412fc4204d2e7542
25LP5306 25LP5306 <25LP5306@pinecones-iMac.local>
2025-08-04 12:35:38 +0800

    pay

---
+++ src/app/payment/page.tsx
@@ -0,0 +1,9 @@
+import { PaymentSection } from "./components/PaymentSection";
+
+export default function Home() {
+  return (
+    <div className="flex ">
+      <PaymentSection />
+    </div>
+  );
+}
