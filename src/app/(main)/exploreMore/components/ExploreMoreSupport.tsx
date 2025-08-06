a4c60366ed9ba9b922123eb467a1d472b4ebd1d2
25LP2339 25LP2339 <25LP2339@pinecones-iMac-2.local>
2025-08-06 12:37:33 +0800

    fixed conflicts

--- src/app/(main)/exploreMore/components/ExploreMoreSupport.tsx
+++ src/app/(main)/exploreMore/components/ExploreMoreSupport.tsx
@@ -2,25 +2,6 @@ import { Button } from "@/components/ui/button";
 import { Donation } from "@/lib/types";
 import { Heart } from "lucide-react";
 
-<<<<<<< Updated upstream:src/app/(main)/exploreMore/components/ExploreMoreSupport.tsx
-export const ExploreMoreSupport = () => {
-    const items = Array.from({ length: 4 });
-  return (
-    <div className="border-[1] rounded-sm w-full mt-8 left-6">
-      <h1 className="font-semibold text-lg mt-7.5 ml-6">Recent Supporters</h1>
-      <div className="flex ml-6 mt-4">
-        <div>
-          <Image src="/Profile.png" alt="Profile" width={40} height={40} />
-        </div>
-        <div className="ml-3">
-          <div className="flex gap-4">
-            <h2 className="text-lg font-semibold">John Doe</h2>
-            <p>bouth $1 coffee</p>
-          </div>
-          <div>
-            <p>Thank you for being so awesome everyday! </p>
-          </div>
-=======
 type RecentSupportersProps = {
   donations?: Donation[];
   onSeeMoreClick: () => void;
@@ -38,7 +19,6 @@ export const ExploreMoreSupport = ({
             <Heart className="fill-black" />
           </span>
           <span> Be the first one to support Jake</span>
->>>>>>> Stashed changes:src/app/(main)/[profileName]/_components/ExploreMoreSupport.tsx
         </div>
       </div>
     );
